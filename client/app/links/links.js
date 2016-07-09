50596eb5a64d23aadb0457f14fdfe58c7ea14473
daredia <shehzad.daredia@gmail.com>
2016-07-08 21:58:14 -0700

    Add sortBy and filter to links template

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -4,10 +4,7 @@ angular.module('shortly.links', [])
   // Your code here
   $scope.data = {};
   Links.getAll().then(function(links) {
-    $scope.data.links = [
-    { "code" : "738dd", "url" : "http://www.google.com", "title" : "Google", "baseUrl" : "http://45.55.4.184:4568", "visits": 1},
-    { "code" : "d76b7", "url" : "http://www.apple.com", "title" : "Apple", "baseUrl" : "http://45.55.4.184:4568", "visits": 7}
-    ];
+    $scope.data.links = links;
   })
   .catch(console.error.bind(console));
 });
