7633097fe233da73e5d6736bcdea011d387a3e1d
daredia <shehzad.daredia@gmail.com>
2016-07-08 20:36:02 -0700

    Implement dynamic links template, add dummy data to links controller, and add nav to index.html

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -4,7 +4,10 @@ angular.module('shortly.links', [])
   // Your code here
   $scope.data = {};
   Links.getAll().then(function(links) {
-    $scope.data.links = links;
+    $scope.data.links = [
+    { "code" : "738dd", "url" : "http://www.google.com", "title" : "Google", "baseUrl" : "http://45.55.4.184:4568", "visits": 1},
+    { "code" : "d76b7", "url" : "http://www.apple.com", "title" : "Apple", "baseUrl" : "http://45.55.4.184:4568", "visits": 7}
+    ];
   })
   .catch(console.error.bind(console));
 });
