cb68eae42e4bee45d716d5db4574904619d4c669
daredia <shehzad.daredia@gmail.com>
2016-07-09 11:15:52 -0700

    Enable nagivation and authentication for links and shorten view, and add logout functionality

--- client/app/app.js
+++ client/app/app.js
@@ -17,13 +17,19 @@ angular.module('shortly', [
     })
     .when('/links', {
       templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+      controller: 'LinksController',
+      authenticate: true
     })
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
-      controller: 'ShortenController'
-    });
-    // Your code here
+      controller: 'ShortenController',
+      authenticate: true
+    })
+    .when('/logout', {
+      templateUrl: 'app/auth/signin.html',
+      controller: 'AuthController'
+    })
+    .otherwise({redirectTo: '/links'});
 
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
