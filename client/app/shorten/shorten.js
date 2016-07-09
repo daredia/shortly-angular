d420015c3a7c0a8f48a67762ca3b044fc0baa37e
daredia <shehzad.daredia@gmail.com>
2016-07-09 16:37:26 -0700

    Validate the shorten links form before it submits to the server

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -6,8 +6,11 @@ angular.module('shortly.shorten', [])
   $scope.isSuccess = false;
   $scope.isError = false;
 
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
   $scope.addLink = function() {
-    Links.addOne({url: $scope.newLink})
+    if ($scope.isValidUrl($scope.newLink)) {
+      Links.addOne({url: $scope.newLink})
       .then(function(response) {
         if (response.status === 500) {
           $scope.isSuccess = false;
@@ -19,5 +22,18 @@ angular.module('shortly.shorten', [])
         }
       })
       .catch(console.error.bind(console));
+    } else {
+      $scope.isSuccess = false;
+      $scope.isError = true;
+    }
+    
+  };
+
+  $scope.isValidUrl = function (url) {
+    return url.match(rValidUrl);
   };
 });
+
+
+
+
